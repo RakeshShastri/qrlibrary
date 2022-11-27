deeb32ecad2723db51dcfc5a929c6725964d27dc
dasashreeya <86039918+dasashreeya@users.noreply.github.com>
2022-11-28 01:43:29 +0530

    Commit

--- app/src/main/java/com/qrlibrary/MainActivity.java
+++ app/src/main/java/com/qrlibrary/MainActivity.java
@@ -2,13 +2,26 @@ package com.qrlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button move;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        move=findViewById(R.id.adminlogin2);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AdminLogin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
